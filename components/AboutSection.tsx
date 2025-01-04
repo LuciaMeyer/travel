dd5166a6f0d2a9463e205d491abd61a806be9573
Lucía Meyer <luciameyer1603@gmail.com>
2025-01-04 19:44:10 -0300

    fix deploy 3

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -49,31 +49,33 @@ export const AboutSection: React.FC<AboutSectionProps> = ({ refAbout }) => {
       className='flex-1 relative overflow-hidden md:mx-auto mx-6 h-full mt-10'
     >
     {data.map((item: any, index: number) => (
-      <SlideUp offset='-300px 0px -300px 0px' key={index}>
-        <motion.section
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          id={`trip-${index}`}
-          className="flex flex-col justify-center md:mb-40 mb-20 mt-10"
-        >
-          <div className="flex flex-col justify-center mb-9 bg-white dark:bg-BGD">
-            <span className="md:text-center" style={{ fontSize: '1rem' }}>{ item.date }</span>
-            <h2 className="md:text-center font-bold text-LM" style={{ fontSize: '2rem' }}>{ item.trip }</h2>
-            <h3 className="md:text-center" style={{ fontSize: '1.5rem' }}>{ item.detail }</h3>
-          </div>
-          <div className="mx-auto mb-4 md:mb-0">
-            <video
-              width="350"
-              height="315"
-              controls
-              className="border border-neutral-300 dark:border-neutral-700"
-            >
-              <source src={item.src} type="video/mp4" />
-            </video>
-          </div>
-        </motion.section> 
-      </SlideUp>
+      <div key={index}>
+        <SlideUp offset='-300px 0px -300px 0px' >
+          <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            id={`trip-${index}`}
+            className="flex flex-col justify-center md:mb-40 mb-20 mt-10"
+          >
+            <div className="flex flex-col justify-center mb-9 bg-white dark:bg-BGD">
+              <span className="md:text-center" style={{ fontSize: '1rem' }}>{ item.date }</span>
+              <h2 className="md:text-center font-bold text-LM" style={{ fontSize: '2rem' }}>{ item.trip }</h2>
+              <h3 className="md:text-center" style={{ fontSize: '1.5rem' }}>{ item.detail }</h3>
+            </div>
+            <div className="mx-auto mb-4 md:mb-0">
+              <video
+                width="350"
+                height="315"
+                controls
+                className="border border-neutral-300 dark:border-neutral-700"
+              >
+                <source src={item.src} type="video/mp4" />
+              </video>
+            </div>
+          </motion.section> 
+        </SlideUp>
+      </div>
     ))}
     </section>
   );
